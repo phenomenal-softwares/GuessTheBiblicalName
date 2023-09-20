a6977be40be22aefd129beee4d325d8ed849ff76
Phenomenal <115654413+phenomenal-softwares@users.noreply.github.com>
2023-09-20 19:47:03 +0100

    Add files via upload

---
+++ js/bible.js
@@ -0,0 +1,18 @@
+const bibleVerse = [
+  "Blessed is every one that feareth the Lord; that walketh in his ways" + "<br>" + "(Psalm 128:1)",
+  "Teach me to do your will, for you are my God! Let your good Spirit lead me on level ground!" + "<br>" + "(Psalm 143:10)",
+  "You are my friends if you do what I command you." + "<br>" + "(John 15:14)",
+  "Iron sharpeneth iron; so a man sharpeneth the countenance of his friend." + "<br>" + "(Proverbs 27:17)",
+  "I can do all things through Christ which strengtheneth me." + "<br>" + "(Philippians 4:13)",
+  "Hear, O Israel: The Lord our God is one Lord:" + "<br>" + "(Deuteronomy 6:4)",
+  "But this I say, He which soweth sparingly shall reap also sparingly; and he which soweth bountifully shall reap also bountifully." + "<br>" + "(2 Corinthians 9:6)",
+  "I know thy works: behold, I have set before thee an open door, and no man can shut it: for thou hast a little strength, and hast kept my word, and hast not denied my name." + "<br>" + "(Revelation 3:8)",
+  "Labour not to be rich: cease from thine own wisdom." + "<br>" + "(Proverbs 23:4)",
+  "Speak not in the ears of a fool: for he will despise the wisdom of thy words." + "<br>" + "(Proverbs 23:9)",
+  "For surely there is an end; and thine expectation shall not be cut off." + "<br>" + "(Proverbs 23:18)",
+  "But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint." + "<br>" + "(Isaiah 40:31)",
+  "I, even I, am the Lord; and beside me there is no saviour." + "<br>" + "(Isaiah 43:11)",
+  "And ye shall seek me, and find me , when ye shall search for me with all your heart." + "<br>" + "(Jeremiah 29:13)",
+  "Humble yourselves in the sight of the Lord, and he shall lift you up." + "<br>" + "(James 4:10)",
+  "Jesus Christ the same yesterday, and to day, and for ever." + "<br>" + "(Hebrews 13:8)"
+  ];
---
+++ js/words.js
@@ -0,0 +1,394 @@
+let wordList = [
+    {
+        word: "dagon",
+        hint: "the god of the Philistines (1Samuel 5:2)"
+    },
+    {
+        word: "damascus",
+        hint: "The capital of Syria where apostle Paul was converted to Christianity on the road (Acts 9)"
+    },
+    {
+        word: "dan",
+        hint: "the fifth son of Jacob (Genesis 30:6)"
+    },
+    {
+        word: "daniel",
+        hint: "he became the second in command of the Babylonian Empire (Daniel 2:48)"
+    },
+    {
+        word: "darius",
+        hint: "it was during his reign that Daniel was thrown into the lion's den (Daniel 6:1)"
+    },
+    {
+        word: "darling",
+        hint: "English word meaning 'beloved' (Psalm 22:20)"
+    },
+    {
+        word: "dathan",
+        hint: "he, along with Korah and Abiram conspired against Moses and was swallowed by an earthquake (Numbers 26:9-10)"
+    },
+    {
+        word: "david",
+        hint: "the youngest son of Jesse, greatest king of Israel (1Samuel 16:13)"
+    },
+    {
+        word: "deacon",
+        hint: "the post appointed by the Apostles with the responsibility to distribute alms, maintain order at meetings and to baptize new converts (1Timothy 3:10)"
+    },
+    {
+        word: "deborah",
+        hint: "the prophetess and judge of Israel, assisted by Barak (Judges 4:4)"
+    },
+    {
+        word: "delilah",
+        hint: "the Philistine woman who cut off Samson's hair and delivered him to his enemies (Judges 16:18)"
+    },
+    {
+        word: "diamond",
+        hint: "one of the precious stones placed in the breastplate of a high priest (Exodus 28:18)"
+    },
+    {
+        word: "diana",
+        hint: "the pagan goddess of the moon and hunting (Acts 19:24)"
+    },
+    {
+        word: "dinah",
+        hint: "the only daughter of Jacob (Genesis 30:21)"
+    },
+    {
+        word: "dorcas",
+        hint: "she is described in the Bible as being full of good works and acts of charity (Acts 9:36-41)"
+    },
+    {
+        word: "dothan",
+        hint: "the residence of the prophet Elisha (2Kings 6:13)"
+    },
+    {
+        word: "drusilla",
+        hint: "the daughter of Herod Agrippa and wife of Felix (Acts 24:24)"
+    },
+    {
+        word: "earnest",
+        hint: "the English word which means pledge (2Corinthians 1:22)"
+    },
+    {
+        word: "ebenezer",
+        hint: "a memorial stone set up by the prophet Samuel to give thanks to God for His help in defeating the Philistines (1Samuel 7:12)"
+    },
+    {
+        word: "ebony",
+        hint: "a fine-grained and durable wood, suitable for carving (Ezekiel 27:15)"
+    },
+    {
+        word: "ed",
+        hint: "the name of a great altar built by the tribes of Reuben and Gad as witness that Jehovah is God (Joshua 22:34)"
+    },
+    {
+        word: "eden",
+        hint: "the place in which God placed the first human beings Adam and Eve (Genesis 2:15)"
+    },
+    {
+        word: "ehud",
+        hint: "the second judge of the Israelites, who delivered the Israelites from the tyranny of Eglon, the king of Moab (Judges 3:15)"
+    },
+    {
+        word: "elah",
+        hint: "the last king of Israel, the father of Hosea (2Kings 17:1)"
+    },
+    {
+        word: "eleazar",
+        hint: "the third son of Aaron, who succeeded his father as the high priest (Leviticus 10:6)"
+    },
+    {
+        word: "eli",
+        hint: "he was the high priest and judge of Israel for forty years (1Samuel 1:3)"
+    },
+    {
+        word: "eliab",
+        hint: "the oldest son of Jesse and brother of David (1Samuel 16:6)"
+    },
+    {
+        word: "eliakim",
+        hint: "the original name of King Jehoiakim of Judah (2Kings 23:34)"
+    },
+    {
+        word: "elias",
+        hint: "the Greek version of the Hebrew name Elijah (Matthew 11:14)"
+    },
+    {
+        word: "eliezer",
+        hint: "the second son of Moses and Zipporah (Exodus 18:4)"
+    },
+    {
+        word: "elihu",
+        hint: "one of the brothers of David and the chief of the tribe of Judah (1Chronicles 27:18)"
+    },
+    {
+        word: "elijah",
+        hint: "the great prophet who was taken directly to heaven in a chariot of fire (2Kings 2:11)"
+    },
+    {
+        word: "elimelech",
+        hint: "the husband of Naomi, and father-in-law of Ruth and Orpah (Ruth 1:2)"
+    },
+    {
+        word: "eliphaz",
+        hint: "the son of Esau (Genesis 34:4)"
+    },
+    {
+        word: "elisabeth",
+        hint: "the wife of the priest Zacharias and mother of John the Baptist (Luke 1:5)"
+    },
+    {
+        word: "elisha",
+        hint: "the disciple and successor of the prophet Elijah (2Kings 2:12)"
+    },
+    {
+        word: "elisheba",
+        hint: "wife of Aaron (Exodus 6:23)"
+    },
+    {
+        word: "elkanah",
+        hint: "the father of the prophet Samuel (1Samuel 1:1)"
+    },
+    {
+        word: "elohim",
+        hint: "first name for God found in the Bible. (Genesis 2:4)"
+    },
+    {
+        word: "elon",
+        hint: "eleventh of the judges of Israel (Judges 12:11)"
+    },
+    {
+        word: "elymas",
+        hint: "the Jewish sorcerer Barjesus who tried to turn the apostles Paul and Barnabas from their faith (Acts 13:11)"
+    },
+    {
+        word: "emerald",
+        hint: "a green coloured precious stone (Exodus 39:11)"
+    },
+    {
+        word: "emmanuel",
+        hint: "symbolic and prophetic name of the Messiah, Jesus Christ, prophesying that He would be born of a virgin and would be 'God with us' (Matthew 1:23)"
+    },
+    {
+        word: "enoch",
+        hint: "a very righteous man who was taken directly to heaven without experiencing death (Genesis 5:24)"
+    },
+    {
+        word: "ephraim",
+        hint: "the second son of Joseph (Genesis 41:52)"
+    },
+    {
+        word: "ephrath",
+        hint: "the name of a place near Bethel where Rachel the wife of Jacob died while giving birth to Benjamin (Genesis 35:16)"
+    },
+    {
+        word: "esther",
+        hint: "she was adopted by her cousin Mordecai, and eventually became the wife of Ahasuerus, the King of Persia (Book of Esther )"
+    },
+    {
+        word: "ethan",
+        hint: "he was distinguished for his wisdom, which was surpassed only by Solomon (1Kings 4:31)"
+    },
+    {
+        word: "eunice",
+        hint: "the mother of Timothy (2Timothy 1:5)"
+    },
+    {
+        word: "aaron",
+        hint: "the first high priest of the Israelites (Exodus 28:1)"
+    },
+    {
+        word: "abba",
+        hint: "Aramaic name of God that means father (Mark 14:36)"
+    },
+    {
+        word: "abda",
+        hint: "the father of Adoniram, who was in charge of the taxes under King Solomon (1Kings 4:6)"
+    },
+    {
+        word: "abednego",
+        hint: "the Aramaic name given to Azariah, one of Daniel's friends (Daniel 3:12-20)"
+    },
+    {
+        word: "abel",
+        hint: "the second son of Adam and Eve (Genesis 4:1-16)"
+    },
+    {
+        word: "abez",
+        hint: "a town belonging to the tribe of Issachar (Joshua 19:20)"
+    },
+    {
+        word: "abi",
+        hint: "the mother of Hezekiah, the king of Judah (2Kings 18:2)"
+    },
+    {
+        word: "bamoth",
+        hint: "a town the Israelites passed through after their exodus from Egypt (Numbers 21:19)"
+    },
+    {
+        word: "abagtha",
+        hint: "one of the seven eunuchs who served as chamberlains in the court of King Ahasuerus of Persia (Esther 1:10)"
+    },
+    {
+        word: "barabbas",
+        hint: "a notorious robber whom Pilate proposed to condemn to death instead of Jesus, whom he wished to release (Mark 15:7)"
+    },
+    {
+        word: "barak",
+        hint: "under the guidance of the Prophetess Deborah, he made war against Jabin, the king of Hazor and delivered Israel from his tyranny (Judges 4:6)"
+    },
+    {
+        word: "barjona",
+        hint: "the surname of the apostle Peter (Matthew 16:17)"
+    },
+    {
+        word: "barnabas",
+        hint: "he introduced the newly converted Paul to the other apostles and later travelled with him on his missionary journeys (Acts 4:36)"
+    },
+    {
+        word: "barsabas",
+        hint: "one of the two nominees to replace Judas Iscariot (Acts 1:23)"
+    },
+    {
+        word: "nathaniel",
+        hint: "Bartholomew's other name (Acts 1:13)"
+    },
+    {
+        word: "bartimeus",
+        hint: "the blind beggar of Jericho (Matthew 20:30)"
+    },
+    {
+        word: "bashemath",
+        hint: "the daughter of Solomon, married to Ahimaaz (1Kings 4:15)"
+    },
+    {
+        word: "bathsheba",
+        hint: "wife of Uriah, mother of Solomon (2Samuel 11:3)"
+    },
+    {
+        word: "beacon",
+        hint: "a banner set as a signal on top of mountains (Isaiah 30:17)"
+    },
+    {
+        word: "becher",
+        hint: "Son of Benjamin and grandson of Jacob (Genesis 46:21)"
+    },
+    {
+        word: "bedan",
+        hint: "One of the judges of Israel, the successor of Gideon (1Samuel 12:11)"
+    },
+    {
+        word: "belah",
+        hint: "the eldest son of Benjamin (Genesis 46:21)"
+    },
+    {
+        word: "ben",
+        hint: "appointed by David to work as a porter at the ark (1Chronicles 15:18)"
+    },
+    {
+        word: "benaiah",
+        hint: "chief bodyguard of David who later became the commander-in-chief of Solomon's army (1Kings 1:38)"
+    },
+    {
+        word: "benjamin",
+        hint: "the youngest son of Jacob and Rachael (Genesis 35:16)"
+    },
+    {
+        word: "bernice",
+        hint: "the eldest daughter of Herod Agrippa, the ruler of Palestine (Acts 25:13)"
+    },
+    {
+        word: "beulah",
+        hint: "a metamorphic name used for the land of Judah, to indicate that it is favored and blessed by the Lord (Isaiah 62:4)"
+    },
+    {
+        word: "caesar",
+        hint: "a title assumed by the Roman Emperors (John 19:15)"
+    },
+    {
+        word: "caesarea",
+        hint: "a town where Cornelius was converted to Christianity by Peter (Acts 10:1)"
+    },
+    {
+        word: "cain",
+        hint: "the eldest son of Adam and Eve (Genesis 4)"
+    },
+    {
+        word: "caleb",
+        hint: "he was sent to Canaan as a spy (Joshua 14:6-7)"
+    },
+    {
+        word: "calvary",
+        hint: "the name of the hill on which Jesus Christ was crucified (Luke 23:33)"
+    },
+    {
+        word: "cana",
+        hint: "a town in Galilee where Jesus did his first miracle (John 2:1-11)"
+    },
+    {
+        word: "canaan",
+        hint: "fourth son of Ham, whose descendants occupied the country to the west of Jordan (Genesis 10:6)"
+    },
+    {
+        word: "carmel",
+        hint: "a place that the prophet Elijah brought back the people to their allegiance to God (1Kings 18)"
+    },
+    {
+        word: "carpenter",
+        hint: "skilled wood craftsman (Isaiah 41:7)"
+    },
+    {
+        word: "chamberlain",
+        hint: "officer in charge of the private apartments of the king (2Kings 23:11)"
+    },
+    {
+        word: "chancellor",
+        hint: "title given to the Persian governor of Samaria (Ezra 4:8)"
+    },
+    {
+        word: "charity",
+        hint: "another name for love, originating from Latin (1Corinthians 13)"
+    },
+    {
+        word: "chloe",
+        hint: "a Christian woman of Corinth who informed Paul of the disputes within the local church (1Corinthians 1:11)"
+    },
+    {
+        word: "christian",
+        hint: "the name given to the followers of Jesus Christ (Acts 11:26)"
+    },
+    {
+        word: "cinnamon",
+        hint: "one of the ingredients of the holy anointing oil (Exodus 30:23)"
+    },
+    {
+        word: "claudia",
+        hint: "a Christian woman at Rome, wife of Pudens (2Timothy 4:31)"
+    },
+    {
+        word: "claudius",
+        hint: "the fourth Roman emperor, who banished all Jews and Christians from Rome (Acts 18:2)"
+    },
+    {
+        word: "clement",
+        hint: "an early Christian convert at Philippi and a fellow-labourer of Paul (Philippians 4:3)"
+    },
+    {
+        word: "corinth",
+        hint: "an ancient city in Greece where Paul established a church (1Corinthians 1:2)"
+    },
+    {
+        word: "cornelius",
+        hint: "he and his family were converted to Christianity by Peter (Acts 10:1)"
+    },
+    {
+        word: "crystal",
+        hint: "a very clear mineral, used to describe the river of life flowing from the throne of God (Revelations 22:1)"
+    },
+    {
+        word: "cyrus",
+        hint: "he conquered Babylon and liberated the captive Jews (Ezra 1:1)"
+    }
+]

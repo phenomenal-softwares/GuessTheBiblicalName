a2ce6711b3e2f1d66c5b61f3b5dee534ead68a21
Phenomenal <115654413+phenomenal-softwares@users.noreply.github.com>
2023-09-22 01:33:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -242,7 +242,7 @@ setTimeout(function() {
 function randomWord() {
  let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
     word = ranItem.word;
-    maxGuesses = word.length >= 5 ? 7 : 5;
+    maxGuesses = word.length >= 5 ? 5 : 3;
     correctLetters = []; incorrectLetters = [];
     hintTag.innerText = ranItem.hint;
     guessLeft.innerText = maxGuesses;
